e877da5fd564bc9d18db155d4c63c511c8e807b8
DIVAKAR SRIVASTAVA <56751176+thedivs@users.noreply.github.com>
2021-05-30 07:37:22 +0530

    Add files via upload

---
+++ EDUTECHFUTURE/contact.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">    
+    <title>EduFuture| Contact</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="image/favicon.ico" type="image/x-icon">
+
+    <!-- Font awesome -->
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">   
+    <!-- Slick slider -->
+    <link rel="stylesheet" type="text/css" href="css/slick.css">          
+    <!-- Fancybox slider -->
+    <link rel="stylesheet" href="css/jquery.fancybox.css" type="text/css" media="screen" /> 
+    <!-- Theme color -->
+    <link id="switcher" href="css/theme-color/default-theme.css" rel="stylesheet">          
+
+    <!-- Main style sheet -->
+    <link href="css/style.css" rel="stylesheet">    
+
+   
+    <!-- Google Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,400italic,300,300italic,500,700' rel='stylesheet' type='text/css'>
+    <script data-ad-client="ca-pub-6503042466667417" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  </head>
+  <body> 
+
+  <!--START SCROLL TOP BUTTON -->
+    <a class="scrollToTop" href="#">
+      <i class="fa fa-angle-up"></i>      
+    </a>
+  <!-- END SCROLL TOP BUTTON -->
+<div>
+<nav class="navbar navbar-dark bg-primary" style="color: white;">
+  <div class="container-md">
+      <span class="navbar-brand" style="font-size: 25px; font-family: sans-serif;">EduTech Future</span>
+      <span class="navbar-brand" style="float: right;">Contact us :<span style="color: black;">&nbsp;+91-9557963838</span></span>
+   
+  </div>
+</nav>
+</div>
+  <!-- Start header  -->
+  <header id="mu-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 col-md-12">
+          <div class="mu-header-area">
+            <div class="row">
+              <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <div class="mu-header-top-left">
+                  <div class="mu-top-email">
+                    <i class="fa fa-envelope"></i>
+                    <span>vikas764122@gmail.com</span>
+                    </div>
+                    <div class="mu-top-phone">
+                    <i class="fa fa-phone"></i>
+                    <span>+91-9557963838</span>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <div class="mu-header-top-right">
+                  <nav>
+<!--                     <ul class="mu-top-social-nav">
+                      <li><a href="#"><span class="fa fa-facebook"></span></a></li>
+                      <li><a href="#"><span class="fa fa-google-plus"></span></a></li>
+                      <li><a href="#"><span class="fa fa-youtube"></span></a></li>
+                    </ul> -->
+                    <a href="index.php" class="pull-right btn logb btn-primary" style="color:white"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>&nbsp;<font style="font-size:12px;font-weight:bold">Home</font></a>
+                  </nav>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </header>
+  <!-- End header  -->
+
+ 
+ <!-- Start contact  -->
+ <section id="mu-contact">
+   <div class="container">
+     <div class="row">
+       <div class="col-md-12">
+         <div class="mu-contact-area">
+          <!-- start title -->
+
+          <div class="mu-title">
+            <h2>Gontact us</h2>
+            <p>Welcome to the EDUTECH FUTURE. We help you in different types of EXAMS through QUIZES. You can prepare yourself by taking Daily Quizes. You can mail us on<b> vikas764122@gmail.com</b> for any suggestions/query. You can call us on <b>+91-9557963838.</b></span></p>
+          </div>
+          <!-- end title -->
+          <!-- start contact content -->
+          <div class="mu-contact-content col-md-12" style="text-align: center;">           
+            <div class="col-md-12">
+              <div class="col-md-12">
+                <div>
+                   <h4><u>Contact</u></h4>
+                <address>
+                  <p>M 5/6 Pragtipuram Colony, Raebareli 229001, Uttar Pradesh </p>
+                  <p>Phone: +919557963838 </p>
+                  <p>Email: vikas764122@gmail.com</p>
+
+                  
+                </address>
+             
+                </div>
+              </div>
+         </div>
+       </div>
+ </section>
+ <!-- End contact  -->
+  
+
+  <!-- Start footer -->
+ <footer id="mu-footer"><!-- start footer top -->
+<div class="mu-footer-top">
+<div class="container">
+<div class="mu-footer-top-area">
+<div class="row">
+<div class="col-lg-3 col-md-3 col-sm-3">
+
+</div>
+<!--     <div class="col-lg-3 col-md-3 col-sm-3">`
+ 
+            </div> -->
+
+<div class="col-lg-3 col-md-3 col-sm-3">
+<div class="mu-footer-widget">
+  
+<h4><u>Contact</u></h4>
+
+<address>
+<p>M 5/6 Pragtipuram Colony, Raebareli 229001, Uttar Pradesh</p>
+
+<p>Phone: +919557963838</p>
+
+<p>Website: <a href="http://www.edutechfuture.com/" style="color: white;">www.edutechfuture.com</a></p>
+
+<p>Email: vikas764122@gmail.com</p>
+</address>
+</div>
+</div>
+
+<div class="col-lg-3 col-md-3 col-sm-3">
+<div class="mu-footer-widget">
+<h4><u>Policy</u></h4>
+
+<address>
+<p><a href="privacy.html" style="text-decoration: none; color: white;">Privacy &amp; Policy</a></p>
+
+<p>Designed :<strong><a href="https://www.instagram.com/divstech/" style="text-decoration: none; color: white;" target="_blank">@DivsTech</a></strong></p>
+</address>
+</div>
+</div>
+
+<div class="col-lg-3 col-md-3 col-sm-3"><iframe allowfullscreen="" frameborder="0" height="300" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14314.637119536123!2d81.2437092239879!3d26.24025823474674!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399ba153e3617d17%3A0xb875dcb516e08116!2sPragati%20Puram%2C%20Raebareli%2C%20Uttar%20Pradesh%20229316!5e0!3m2!1sen!2sin!4v1568002304419!5m2!1sen!2sin" style="border:0;" width="300"></iframe></div>
+</div>
+</div>
+</div>
+<!-- end footer top --><!-- jQuery library --><script src="js/jquery.min.js"></script><!-- Include all compiled plugins (below), or include individual files as needed --><script src="js/bootstrap.js"></script><!-- Slick slider --><script type="text/javascript" src="js/slick.js"></script><!-- Counter --><script type="text/javascript" src="js/waypoints.js"></script><script type="text/javascript" src="js/jquery.counterup.js"></script><!-- Mixit slider --><script type="text/javascript" src="js/jquery.mixitup.js"></script><!-- Add fancyBox --><script type="text/javascript" src="js/jquery.fancybox.pack.js"></script><!-- Custom js --><script src="js/custom.js"></script></div>
+</footer>
+
+  </body>
+</html>
---
+++ EDUTECHFUTURE/testing.php
@@ -0,0 +1,19 @@
+ <?php
+include 'dbConnection.php';
+$result = mysqli_query($con, "SELECT * FROM quiz WHERE status = 'enabled' AND category= 'Physics' ORDER BY date DESC") or die('Error');
+    echo '<div class="content panel" ><table class="table table-striped title1" >
+<tr><td style="vertical-align:middle"><b>S.N.</b></td><td style="vertical-align:middle"><b>Name</b></td><td style="vertical-align:middle"><b>Total question</b></td><td style="vertical-align:middle"><b>Correct Answer</b></td><td style="vertical-align:middle"><b>Wrong Answer</b></td><td style="vertical-align:middle"><b>Total Marks</b></td><td style="vertical-align:middle"><b>Time limit</b></td><td style="vertical-align:middle"><b>Action</b></td></tr>';
+    $c = 1;
+    while ($row = mysqli_fetch_array($result)) {
+        $title   = $row['title'];
+        $total   = $row['total'];
+        $correct = $row['correct'];
+        $wrong   = $row['wrong'];
+        $time    = $row['time'];
+        // $eid     = $row['eid'];
+       
+            echo '<tr><td style="vertical-align:middle">' . $c++ . '</td><td style="vertical-align:middle">' . $title . '</td><td style="vertical-align:middle">' . $total . '</td><td style="vertical-align:middle">+' . $correct . '</td><td style="vertical-align:middle">-' . $wrong . '</td><td style="vertical-align:middle">' . $correct * $total . '</td><td style="vertical-align:middle">' . $time . '&nbsp;min</td>
+  <td style="vertical-align:middle"><b><a href="account.php?q=quiz' . $total . '&start=start" class="btn" style="color:#FFFFFF;background:darkgreen;font-size:12px;padding:7px;padding-left:10px;padding-right:10px"><span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>&nbsp;<span><b>Start</b></span></a></b></td></tr>';
+
+        }
+?>
